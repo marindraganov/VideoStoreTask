1839cffea5d8db04a224679f22bf1b1e9853587b
Marin <marodraganov@gmail.com>
2020-02-03 21:06:40 +0200

    remove redundant variables

--- VideoStore/Customer.cs
+++ VideoStore/Customer.cs
@@ -27,37 +27,36 @@ namespace VideoStore
         {
             double totalAmount = 0;
             int frp = 0;
-            var theItems = this.rentals.ToArray();
+
             // statement text will be appended here
             string report = $"{this.Name}'s Rental Record\n";
-            for (int i = 0; i < theItems.Length; i++)
+            foreach (var rental in this.rentals)
             {
-                Rental each = theItems[i];
                 double thisAmount = 0;
                 //determine amounts for each line
-                switch (each.Movie.PriceCode)
+                switch (rental.Movie.PriceCode)
                 {
                     case PriceCode.Regular:
                         thisAmount += 2;
-                        if (each.DaysRented > 2)
-                            thisAmount += (each.DaysRented - 2) * 1.5;
+                        if (rental.DaysRented > 2)
+                            thisAmount += (rental.DaysRented - 2) * 1.5;
                         break;
                     case PriceCode.NewRelease:
-                        thisAmount += each.DaysRented * 3;
+                        thisAmount += rental.DaysRented * 3;
                         break;
                     case PriceCode.Kids:
                         thisAmount += 1.5;
-                        if (each.DaysRented > 3)
-                            thisAmount += (each.DaysRented - 3) * 1.5;
+                        if (rental.DaysRented > 3)
+                            thisAmount += (rental.DaysRented - 3) * 1.5;
                         break;
                 }
 
                 // add frequent renter points
                 frp++;
                 // add bonus for a two day new release rental
-                if ((each.Movie.PriceCode == PriceCode.NewRelease) && each.DaysRented > 1) frp++;
+                if ((rental.Movie.PriceCode == PriceCode.NewRelease) && rental.DaysRented > 1) frp++;
                 //show figures for this rental
-                report += $"\t{each.Movie.Title}\t{thisAmount}\n";
+                report += $"\t{rental.Movie.Title}\t{thisAmount}\n";
                 totalAmount += thisAmount;
             }
 
